acba995595d13e9ad8eed7e63adaf582ae18176c
SAMANTHITO <alfons17ver@gmail.com>
2024-02-29 11:09:50 -0500

    antes de crar los modulos

--- src/app/empleados/empleado/empleado.component.ts
+++ src/app/empleados/empleado/empleado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IEmpleado } from '../interfaces/empleado';
 
 @Component({
   selector: 'app-empleado',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./empleado.component.css']
 })
 export class EmpleadoComponent {
-
+  tituloAntiguo:string = 'Empleados Antiguos';
+  tituloNuevo:string = 'Empleados nuevos';
+  empleadosAntiguos:IEmpleado[]=[
+    {
+      nombres:'luisito',
+      apellidos:'comunica',
+      direccion:'ciudad de mexico',
+      sueldo: 4000
+    },
+    {
+      nombres:'vegueta',
+      apellidos:'777',
+      direccion:'barcelona',
+      sueldo: 6000
+    },
+    {
+      nombres:'auron',
+      apellidos:'play',
+      direccion:'españa',
+      sueldo: 5000
+    },
+    {
+      nombres:'ibai',
+      apellidos:'llanos',
+      direccion:'madrid',
+      sueldo: 10000
+    },
+    {
+      nombres:'mariana',
+      apellidos:'perez',
+      direccion:'mexico',
+      sueldo: 7500
+    }
+  ];
+ 
+  empleadosNuevos:IEmpleado[]=[
+    {
+      nombres:'fernan',
+      apellidos:'floo',
+      direccion:'el salvador',
+      sueldo: 5000
+    }
+  ]
 }
--- src/app/empleados/listado/listado.component.ts
+++ src/app/empleados/listado/listado.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IEmpleado } from '../interfaces/empleado';
 
 @Component({
   selector: 'app-listado',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent {
+  etiquetaBorrado: string = '';
 
+  @Input()
+  empleados: IEmpleado[]=[];
+
+  @Input()
+  titulo:string = '';
+
+  borrar(){
+    //console.log('borrado');
+    const emp = this.empleados.shift();
+    if(emp !== undefined){
+      this.etiquetaBorrado = emp?.nombres+' '+emp?.apellidos;
+    }else{
+      this.etiquetaBorrado = '';
+    }
+    
+  }
 }
